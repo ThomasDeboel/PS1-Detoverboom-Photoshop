233cf35e432eebe424dde50d1fd695f1fee13549
Thomas Deboel <r0841840@student.thomasmore.be>
2023-04-12 17:00:42 +0200

    code verbetert

--- sjabloon.js
+++ sjabloon.js
@@ -1,6 +1,7 @@
 const sliderBlur = document.getElementById("rangeBlur");
 const sliderZwartWit = document.getElementById("rangeZwartWit");
 const sliderContrast = document.getElementById("rangeContrast");
+const sliderDraaien = document.getElementById("rangeDraaien");
 
 const image = document.getElementById("foto1");
 
@@ -13,14 +14,13 @@ sliderZwartWit.addEventListener('input', function() {
 sliderContrast.addEventListener('input', function () {
     image.style.filter = `blur(${sliderBlur.value}px) grayscale(${sliderZwartWit.value}%) contrast(${this.value*10}%)`;
 });
+sliderDraaien.addEventListener('input', function () {
+    image.style.filter = `blur(${sliderBlur.value}px) grayscale(${sliderZwartWit.value}%) contrast(${sliderContrast.value*10}%)`;
+    image.style.transform = `rotate(${this.value*10}deg)`;
+});
 
-/*
 // select all buttons inside #buttongroup
 const buttons = document.querySelectorAll('#buttongroup button');
 console.log('buttons', buttons);
 
 // loop over the buttons inside the NodeList
-buttons.forEach(function (button, index) {
-    // log information about every button
-    console.log('button', index, button);
-});*/
