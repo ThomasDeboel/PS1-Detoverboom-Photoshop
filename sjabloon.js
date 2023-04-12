cb7b1bf98a1f2f7ceafdad41135734fec9fbac71
Thomas Deboel <r0841840@student.thomasmore.be>
2023-04-12 20:52:05 +0200

    de knoppen verbergen werkt nu ook

--- sjabloon.js
+++ sjabloon.js
@@ -5,21 +5,78 @@ const sliderDraaien = document.getElementById("rangeDraaien");
 
 const image = document.getElementById("foto1");
 
+sliderZwartWit.style.display = 'none';
+sliderDraaien.style.display ='none';
+sliderContrast.style.display ='none';
+sliderBlur.style.display ='none';
+
+// if you change this slider than the blur will wil increase or decrease
 sliderBlur.addEventListener('input', function() {
     image.style.filter = `blur(${this.value}px) grayscale(${sliderZwartWit.value}%) contrast(${sliderContrast.value*10}%)`;
 });
+// if you change this slider than the zwartwit value will wil increase or decrease
 sliderZwartWit.addEventListener('input', function() {
     image.style.filter = `blur(${sliderBlur.value}px) grayscale(${this.value}%) contrast(${sliderContrast.value*10}%)`;
 });
+// if you change this slider than the contrast will wil increase or decrease
 sliderContrast.addEventListener('input', function () {
     image.style.filter = `blur(${sliderBlur.value}px) grayscale(${sliderZwartWit.value}%) contrast(${this.value*10}%)`;
 });
+// if you change the value of the slider than the picture will turn
 sliderDraaien.addEventListener('input', function () {
     image.style.filter = `blur(${sliderBlur.value}px) grayscale(${sliderZwartWit.value}%) contrast(${sliderContrast.value*10}%)`;
     image.style.transform = `rotate(${this.value*10}deg)`;
 });
 
-// select all buttons inside #buttongroup
-const buttons = document.querySelectorAll('#buttongroup button');
+// select of all the buttons
+const buttonblackwhite = document.querySelector('#zwartwit');
+const buttonblur = document.querySelector('#blur');
+const buttoncontrast = document.querySelector('#contrast');
+const buttondraaien = document.querySelector('#draaien');
 
-// loop over the buttons inside the NodeList
+// if the buttonblackwhite is pressed than it checks if the slider is visible it hides it and if it is not visible it hides all the other sliders
+buttonblackwhite.addEventListener('click', function (){
+    if(sliderZwartWit.style.display ==='none') {
+        sliderZwartWit.style.display ='flex';
+        sliderDraaien.style.display ='none';
+        sliderContrast.style.display ='none';
+        sliderBlur.style.display ='none';
+    }
+    else{
+        sliderZwartWit.style.display ='none';
+    }
+});
+buttonblur.addEventListener('click', function (){
+    if(sliderBlur.style.display ==='none') {
+        sliderZwartWit.style.display ='none';
+        sliderDraaien.style.display ='none';
+        sliderContrast.style.display ='none';
+        sliderBlur.style.display ='flex';
+    }
+    else{
+        sliderBlur.style.display ='none';
+    }
+});
+buttoncontrast.addEventListener('click', function (){
+    if(sliderContrast.style.display ==='none') {
+        sliderZwartWit.style.display ='none';
+        sliderDraaien.style.display ='none';
+        sliderContrast.style.display ='flex';
+        sliderBlur.style.display ='none';
+    }
+    else{
+        sliderContrast.style.display ='none';
+    }
+});
+buttondraaien.addEventListener('click', function (){
+    if(sliderDraaien.style.display ==='none') {
+        sliderZwartWit.style.display ='none';
+        sliderDraaien.style.display ='flex';
+        sliderContrast.style.display ='none';
+        sliderBlur.style.display ='none';
+    }
+    else{
+        sliderDraaien.style.display ='none';
+    }
+});
+
