538447611ec6e983984e53fcbe5d39bbdc55a17b
Thomas Deboel <r0841840@student.thomasmore.be>
2023-04-15 17:11:59 +0200

    quick fix

--- ZwartWit.js
+++ ZwartWit.js
@@ -1,5 +1,17 @@
-const sliderZwartWit = document.getElementById("rangeZwartWit3");
+const sliderZwartWit3 = document.getElementById("rangeZwartWit3");
+const sliderZwartWit = document.getElementById("rangeZwartWit");
 const image = document.getElementById("foto1");
-sliderZwartWit.addEventListener('input', function() {
+
+sliderZwartWit.style.display ='none';
+sliderZwartWit3.addEventListener('input', function() {
     image.style.filter = `grayscale(${this.value*10}%)`;
+});
+const buttonblackwhite = document.querySelector('#zwartwit');
+buttonblackwhite.addEventListener('click', function (){
+    if(sliderZwartWit.style.display ==='none') {
+        sliderZwartWit.style.display ='flex';
+    }
+    else{
+        sliderZwartWit.style.display ='none';
+    }
 });
