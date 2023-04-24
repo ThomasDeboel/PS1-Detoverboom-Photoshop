60ecb00f666ec56c4383f422d9b8dc1192f90e30
Thomas Deboel <r0841840@student.thomasmore.be>
2023-04-24 15:30:19 +0200

    zwartwitstring

--- sjabloon.js
+++ sjabloon.js
@@ -102,3 +102,23 @@ buttonbrightness.addEventListener('click', function (){
     }
 });
 
+const oefening1 = document.getElementById("oefening1");
+const computedstyle = window.getComputedStyle(oefening1);
+const filter1 = computedstyle.getPropertyValue('filter');
+
+const Alertbtn = document.getElementById('controleren');
+const myElement = document.getElementById("alert")
+
+Alertbtn.addEventListener('click',function (){
+
+    let zwartwitstring = "grayscale(" + sliderZwartWit.value + ")";
+    myElement.style.display = "none";
+    if (filter1.includes(zwartwitstring)){
+        window.location.href = "oefening2.html";
+        console.log(filter1)
+        console.log(zwartwitstring)
+    }
+    else{
+        myElement.style.display = "block";
+    }
+});
