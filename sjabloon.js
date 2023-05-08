7db96a20d715e32a2417fb2b44482500ea143c7d
Thomas Deboel <r0841840@student.thomasmore.be>
2023-05-08 13:30:32 +0200

    oefening 1+l3-4 oef 2  controleer knop werkt

--- sjabloon.js
+++ sjabloon.js
@@ -102,9 +102,13 @@ buttonbrightness.addEventListener('click', function (){
     }
 });
 
-const oefening1 = document.getElementById("oefening1");
+/*const oefening1 = document.getElementById("oefening1");
 const computedstyle = window.getComputedStyle(oefening1);
 const filter1 = computedstyle.getPropertyValue('filter');
+
+// const oefening2L3 = documument.getElementById("oef2")
+// const computedstyleoef2 = window.getComputedStyle(oefening2L3);
+// const filter2L3 = computedstyleoef2.getPropertyValue('filter');
 
 const Alertbtn = document.getElementById('controleren');
 const myElement = document.getElementById("alert")
@@ -112,13 +116,17 @@ const myElement = document.getElementById("alert")
 Alertbtn.addEventListener('click',function (){
 
     let zwartwitstring = "grayscale(" + sliderZwartWit.value + ")";
-    myElement.style.display = "none";
+    // myElement.style.display = "none";
     if (filter1.includes(zwartwitstring)){
         window.location.href = "oefening2.html";
-        console.log(filter1)
-        console.log(zwartwitstring)
     }
     else{
-        myElement.style.display = "block";
+        // myElement.style.display = "block";
     }
-});
+    // let blur = "blur(" + sliderBlur.value + "px)";
+    // let contrast = "contrast("+sliderContrast.value*10 + "%)";
+    // console.log(blur)
+    // if (filter2L3.includes(blur) && filter2L3.includes(contrast)){
+    //     window.location.href = "oefening3.html"
+    // }
+});*/
