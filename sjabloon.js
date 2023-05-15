430c3d3470cafc8ae20a55817c333187871ac86b
VicDK <r0946943@student.thomasmore.be>
2023-05-15 11:10:17 +0200

    tekst op afbeelding

--- sjabloon.js
+++ sjabloon.js
@@ -3,7 +3,11 @@ const sliderZwartWit = document.getElementById("rangeZwartWit");
 const sliderContrast = document.getElementById("rangeContrast");
 const sliderDraaien = document.getElementById("rangeDraaien");
 const sliderBrightness = document.getElementById("rangeBrightness");
-
+const foto = document.getElementById('foto1');
+let tekstInput = document.getElementById('tekst-input');
+let tekstInvoer = document.getElementById('tekst-invoer');
+const draggable = document.getElementById('draggable-element');
+const buttonTekst = document.getElementById('tekst');
 const image = document.getElementById("foto1");
 
 sliderZwartWit.style.display = 'none';
@@ -99,6 +103,62 @@ buttonbrightness.addEventListener('click', function (){
     }
     else{
         sliderBrightness.style.display = 'none';
+    }
+});
+
+var selected = null, // Object of the element to be moved
+    x_pos = 0,
+    y_pos = 0, // Stores x & y coordinates of the mouse pointer
+    x_elem = 0,
+    y_elem = 0; // Stores top, left values (edge) of the element
+
+// Will be called when user starts dragging an element
+function _drag_init(elem) {
+    // Store the object of the element which needs to be moved
+    selected = elem;
+    x_elem = x_pos - selected.offsetLeft;
+    y_elem = y_pos - selected.offsetTop;
+}
+
+// Will be called when user dragging an element
+function _move_elem(e) {
+    x_pos = document.all ? window.event.clientX : e.pageX;
+    y_pos = document.all ? window.event.clientY : e.pageY;
+    if (selected !== null) {
+        selected.style.left = (x_pos - x_elem) + 'px';
+        selected.style.top = (y_pos - y_elem) + 'px';
+    }
+}
+
+// Destroy the object when we are done
+function _destroy() {
+    selected = null;
+}
+
+// Bind the functions...
+document.getElementById('draggable-element').onmousedown = function() {
+    _drag_init(this);
+    return false;
+};
+
+document.onmousemove = _move_elem;
+document.onmouseup = _destroy;
+tekstInvoer.addEventListener('change', () => {
+    const nieuweTekst = tekstInvoer.value;
+    draggable.textContent = nieuweTekst;
+    draggable.classList.remove('verborgen');
+});
+
+buttonTekst.addEventListener('click', function (){
+    if(tekstInput.style.display ==='none') {
+        sliderZwartWit.style.display ='none';
+        tekstInput.style.display ='flex';
+        sliderContrast.style.display ='none';
+        sliderBlur.style.display ='none';
+        sliderBrightness.style.display = 'none';
+    }
+    else{
+        tekstInput.style.display ='none';
     }
 });
 
